8a8dc53656c7ce39b7d44284b78897615347dd26
isaacsutor <isaac.sutor@gmail.com>
2019-01-19 17:57:50 -0500

    qt set up

---
+++ qt_intro.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+
+window.show()
+
+# sys.exit(app.exec_())
+
+
