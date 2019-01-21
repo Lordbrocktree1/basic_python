cdb5a850f40cdd02532f77b2400e764a0d8fcb60
isaacsutor <isaac.sutor@gmail.com>
2019-01-21 18:14:00 -0500

    Example button and tip on hover added

--- qt_intro.py
+++ qt_intro.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
 from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QFont
 from PyQt5.QtCore import pyqtSlot
 
 
@@ -12,6 +13,16 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50,50)
+
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Icon')
         # self.setWindowIcon(QIcon('web.png'))
@@ -24,11 +35,6 @@ if __name__ == '__main__':
     sys.exit(app.exec_())
 
 
-# w = QWidget()
-# w.resize(250, 150)
-# w.move(300, 300)
-# w.setWindowTitle('Simple')
-# w.show()
 
 
 
