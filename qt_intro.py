2f85afc204a604101e346bb0bb6b59e6d237c271
isaacsutor <isaac.sutor@gmail.com>
2019-01-21 18:09:48 -0500

    Object Oriented Simple Setup

--- qt_intro.py
+++ qt_intro.py
@@ -3,12 +3,32 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
-app = QApplication(sys.argv)
-w = QWidget()
-w.resize(250, 150)
-w.move(300, 300)
-w.setWindowTitle('Simple')
-w.show()
 
-sys.exit(app.exec_())
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        # self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+# w = QWidget()
+# w.resize(250, 150)
+# w.move(300, 300)
+# w.setWindowTitle('Simple')
+# w.show()
+
+
 
