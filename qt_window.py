efd68956f7fc9d80494b23d622790f7fce460dba
isaacsutor <isaac.sutor@gmail.com>
2019-01-23 17:46:27 -0500

    file and exit added On mac ensure using space before & as words reserved

--- qt_window.py
+++ qt_window.py
@@ -1,12 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, \
+    QAction, qApp
+from PyQt5.QtGui import QIcon
+
 
 class Example(QMainWindow):
+
     def __init__(self):
         super().__init__()
+
         self.initUI()
+
     def initUI(self):
-        self.statusBar().showMessage('Ready')
+
+        exitAct = QAction(QIcon('exitpic.png'), ' &Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu(' &File')
+        fileMenu.addAction(exitAct)
+
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('Statusbar')
         self.show()
